56d19c38f5ecc05cf296fa3e61f8136d444ef7b6
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-04-10 08:47:00 -0300

    Add BooleanToTextConverter

---
+++ Core/Converters/BooleanToTextConverter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RimSharp.Core.Converters
+{
+    public class BooleanToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && parameter is string param)
+            {
+                var parts = param.Split('|');
+                if (parts.Length == 2)
+                {
+                    return boolValue ? parts[1] : parts[0];
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
